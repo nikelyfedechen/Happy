023e63827837fa4e2e99316ed1ba9091dbb4aa3c
Nikely Fedechen <fedechenikely@gmail.com>
2020-10-19 21:55:41 -0300

    first commit

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,50 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+// create map
+const map = L.map('mapid', options).setView([-3.7333297,-38.5312084], 15)
+
+// create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+
+// create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+// create and add marker
+L.marker([-3.7333297,-38.5312084], { icon })
+.addTo(map)
+
+// image gallery
+function selectImage(event) {
+    const button = event.currentTarget
+
+    console.log(button.children)
+
+    // remover todas as classes .active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button) {
+        button.classList.remove("active")
+    }
+
+    // selecionar a image clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    // atualizar o container de image
+    imageContainer.src = image.src
+    
+    // adicionar a classe .active para este botão
+    button.classList.add('active')
+}
